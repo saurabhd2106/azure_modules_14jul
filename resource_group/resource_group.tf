694747bc8f0334291eb221af1c11d1fed6db053b
Saurabh <saurabh.d2106@gmail.com>
2024-07-14 18:33:15 +0530

    Added Resource Group Module

---
+++ resource_group/resource_group.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "resource_group" {
+
+  name     = var.name
+  location = var.location
+
+  tags = var.tags
+}
+
+output "resource_group" {
+  value = azurerm_resource_group.resource_group
+}
+
+
+output "resource_group_id" {
+  value = azurerm_resource_group.resource_group.id
+}
